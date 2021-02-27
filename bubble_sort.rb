66a29eb696fd56c6c0403362c1c7711034b15529
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-02-27 19:43:34 +0000

    Completed bubble_sort.rb

---
+++ bubble_sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(array)
+  idx = 0
+  (array.length - 1).times do
+    (array.length - 1).times do
+      if array[idx + 1] < array[idx]
+        array[idx], array[idx + 1] = array[idx + 1], array[idx]
+      else
+        array[idx]
+      end
+      idx += 1
+    end
+    idx = 0
+  end
+  p array
+end
+
+bubble_sort([4,3,78,2,0,2])
